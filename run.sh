0e24ef85bb72123a33fcc99b045b1e26bbbc465a
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-12-07 22:01:29 +0100

    pear fixed

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chown www-data:www-data /app -R
-copy /php5conf.d/*.ini /etc/php5/apache2/conf.d/
+cp -f /php5conf.d/*.ini /etc/php5/apache2/conf.d/
 
 if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
     unset ALLOW_OVERRIDE
